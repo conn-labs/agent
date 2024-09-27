839cc7497336eaf49cec3da6ad1ab138b6e8a590
apoorv singh <apoorvsingh@apoorvs-Laptop.local>
2024-09-28 01:12:30 +0530

    feat: fix

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ import { JSDOM } from "jsdom";
 
 import { executeAgent } from "./llm/browser/execute";
 
-await executeAgent("Go to google.com, search google finance, go to google finance, search apple stocks and complete ur workflow by telling me apple stock price from it", "", "one");
+await executeAgent("go to airbnb search varanasi", "", "one");
