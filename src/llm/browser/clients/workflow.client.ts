ab179dc7c0542ae6a0510843f3d95f0ecd3c9ff5
apoorv singh <tofu.develops@gmail.com>
2024-10-06 07:01:20 +0530

    feat: hardest part

--- src/llm/browser/clients/workflow.client.ts
+++ src/llm/browser/clients/workflow.client.ts
@@ -1,5 +1,132 @@
 import MemoryClient from "mem0ai"
+import { BrowserInstance } from "../browser";
+import OpenAI from "openai";
+import { workflowPrompt } from "../../prompt/workflow";
+import { Elements } from "../../../types/browser";
+import { imgToBase64 } from "../../../utils/img";
+import { highlightAndLabelElements } from "../highlight";
+import { AgentAction } from "../../../types/action";
+import { executeAgentAction } from "../actions";
+import { sleep } from "bun";
+import { llmRequest } from "../llm";
+import { waitForEvent } from "../event";
 
 const memory = new MemoryClient(process.env.MEMO || "");
 
 
+export async function workflowAgent(input: string, context: string, memory: boolean = true, instances: number, sessionId: string ) {
+const browser = await BrowserInstance()
+
+const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+      role: "system",
+      content: workflowPrompt(context, instances),
+    },
+    {
+      role: "user",
+      content: input,
+    },
+  ];
+   
+  let elements: Elements[] = [];
+  let screenshot: string = "";
+  let screenshotTaken: boolean = false;
+  let url: string | null = null;
+  let screenshotHash: number = 1;
+  const page = await browser.newPage();
+
+
+  while (true) {
+    if (url) {
+      console.log("URL", url);
+      await page.goto(url, {
+        waitUntil: "domcontentloaded",
+        timeout: 5000,
+      });
+
+      await Promise.race([waitForEvent(page, "load"), sleep(5000)]);
+
+      elements = await highlightAndLabelElements(page);
+      await page.screenshot({
+        path: `${sessionId}-${screenshotHash}.jpg`,
+        fullPage: true,
+      });
+      screenshotTaken = true;
+      screenshot = await imgToBase64(`${sessionId}-${screenshotHash}.jpg`);
+      url = null;
+      screenshotHash++;
+    }
+
+    if (screenshotTaken) {
+      messages.push({
+        role: "user",
+        content: [
+          {
+            type: "image_url",
+            image_url: {
+              url: screenshot,
+              detail: "high",
+            },
+          },
+          {
+            type: "text",
+            text: "Here's the screenshot now continue the workflow accurately and complete further steps",
+          },
+        ],
+      });
+
+      screenshot = "";
+      screenshotTaken = false;
+    }
+
+    const response = await llmRequest(messages);
+
+    if (!response) break;
+
+    messages.push({
+      role: "assistant",
+      content: response.toString(),
+    });
+
+    const data: any = JSON.parse(response.toString());
+
+    if (data.url) {
+      url = data.url;
+    }
+
+    if (data.success) {
+      console.log(data.success);
+      break;
+    }
+    if (data.actions) {
+      const orignalUrl = await new URL(page.url());
+      const mem: String | null | undefined = await executeAgentAction(
+        page,
+        data.actions as AgentAction[],
+        elements,
+      );
+
+      await sleep(4000);
+      const newUrl = await new URL(page.url());
+
+      if (orignalUrl.toString() !== newUrl.toString()) {
+        elements = await highlightAndLabelElements(page);
+
+        await page.screenshot({
+          path: `${sessionId}-${screenshotHash}.jpg`,
+          fullPage: true,
+        });
+        screenshotTaken = true;
+        screenshot = await imgToBase64(`${sessionId}-${screenshotHash}.jpg`);
+        url = null;
+        screenshotHash++;
+        console.log("urls not same");
+      }
+
+      console.log(newUrl.toString());
+      console.log("Mem", mem);
+    }
+  }
+
+  
+}
