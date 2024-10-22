33278c7bf9a879ccfe89c0e5ddedbf97e9c5a607
apoorv singh <tofu.develops@gmail.com>
2024-10-22 20:54:57 +0530

    feat: first commit from new mac

--- src/api/sockets/echo.ts
+++ src/api/sockets/echo.ts
@@ -4,3 +4,6 @@ export default function echoHandler(ws: WebSocket, data: any): void {
   // Echo the data back to the sender
   ws.send(JSON.stringify({ route: "/echo", data }));
 }
+
+
+
--- src/common/htmlParser.ts
+++ src/common/htmlParser.ts
@@ -39,3 +39,4 @@ export async function parseLinksFromHtml(url: string): Promise<LinkInfo[]> {
     return [];
   }
 }
+
